0b92970cb40ee8b458f21726020bab44b9d01bae
hinakanani <hinakanani02@gmail.com>
2021-09-07 17:07:16 +0530

    - Added new method for test

--- Animal.java
+++ Animal.java
@@ -8,10 +8,15 @@ public class Animal {
         System.out.println("I'm running!");
     }
 
+    public void newChange() {
+        System.out.println("Hi this is new method for test");
+    }
+
     public static void main(String[] args){
         Animal charlie = new Animal();
         charlie.eat();
         charlie.run();
+        charlie.newChange();
     }
 
 }
