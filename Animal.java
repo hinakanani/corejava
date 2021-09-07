fc6eb3eff06f137d8c8de8eae3dff49c70193bfc
hinakanani <hinakanani02@gmail.com>
2021-09-07 16:55:10 +0530

    Added Animal class

---
+++ Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+
+    public void eat() {
+        System.out.println("I'm eating");
+    }
+
+    public void run() {
+        System.out.println("I'm running");
+    }
+
+    public static void main(String[] args){
+        Animal charlie=new Animal();
+        charlie.eat();
+        charlie.run();
+    }
+
+}
